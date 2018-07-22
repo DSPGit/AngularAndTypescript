b298cdc0007d0e4bfdf15cd84cb5295b2b7f1871
Dattaprasad Patil <Dattaprasad.Patil@synechron.com>
2018-07-22 15:59:45 +0530

    Day 2: post lunch session

--- Angular/ng5/ng5-webpack-template/vendor.ts
+++ Angular/ng5/ng5-webpack-template/vendor.ts
@@ -1,9 +1,13 @@
 //angular packages
-import '@angular/common'
-import '@angular/core'
-import '@angular/platform-browser'
-import '@angular/platform-browser-dynamic'
+import '@angular/common';
+import '@angular/core';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
 
-import 'rxjs'
+import 'rxjs';
 
 //third party packages [jQuery/bootstrap]
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'popper.js'
