f10b76318372cf7cbcb962cb248305cfcca68c31
Dattaprasad Patil <Dattaprasad.Patil@synechron.com>
2018-07-22 13:24:51 +0530

    Day2: Angular base project

---
+++ Angular/ng5/ng5-webpack-template/vendor.ts
@@ -0,0 +1,9 @@
+//angular packages
+import '@angular/common'
+import '@angular/core'
+import '@angular/platform-browser'
+import '@angular/platform-browser-dynamic'
+
+import 'rxjs'
+
+//third party packages [jQuery/bootstrap]
